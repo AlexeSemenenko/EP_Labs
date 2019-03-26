73c9fe214c00a42ac80503ce12879664e1d8b8c5
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-03-26 19:00:48 +0300

    Add files via upload
    add the first interface

---
+++ Vigenere_cipher/vigenere_cipher.h
@@ -0,0 +1,32 @@
+﻿
+// vigenere_cipher.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CvigenerecipherApp:
+// Сведения о реализации этого класса: vigenere_cipher.cpp
+//
+
+class CvigenerecipherApp : public CWinApp
+{
+public:
+	CvigenerecipherApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CvigenerecipherApp theApp;
